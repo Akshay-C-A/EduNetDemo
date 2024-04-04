5b4504eca726514361eded98cf5f4f783528cd05
Akshay-C-A <akshayanil3103@gmail.com>
2024-04-05 04:49:01 +0530

    deleted current authpage

--- edunetdemo/lib/main.dart
+++ edunetdemo/lib/main.dart
@@ -1,7 +1,7 @@
 // main.dart
 // import 'package:edunetdemo/alumni/alumni_dashboard.dart';
 import 'package:edunetdemo/alumni/alumni_dashboard.dart';
-import 'package:edunetdemo/auth_screen.dart';
+// import 'package:edunetdemo/auth_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:permission_handler/permission_handler.dart';
