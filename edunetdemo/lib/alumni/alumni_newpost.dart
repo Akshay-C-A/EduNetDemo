29703cfca226b903805b4f9140beca3e6afe9562
Akshay-C-A <akshayanil3103@gmail.com>
2024-04-06 21:58:08 +0530

    added loading screen when submitting post

--- edunetdemo/lib/alumni/alumni_newpost.dart
+++ edunetdemo/lib/alumni/alumni_newpost.dart
@@ -29,6 +29,7 @@ class _AlumniNewPostPageState extends State<AlumniNewPostPage> {
   ];
   XFile? _selectedImage;
   final _picker = ImagePicker();
+  bool _isLoading = false;
 
   Future<String?> _uploadImage() async {
     if (_selectedImage == null) return null;
@@ -72,20 +73,34 @@ class _AlumniNewPostPageState extends State<AlumniNewPostPage> {
   }
 
   Future<void> _submitPost() async {
+    setState(() {
+      _isLoading = true;
+    });
+
     final imageURL = await _uploadImage();
     await _AlumniFirestoreService.addAlumniPosts(
       type: _postType,
-      //Added the alumni details from constructor parameter
       alumniId: widget.alumni.alumniId,
-      alumniName: widget.alumni.alumni_name, // Replace with the actual alumni name
-      alumniDesignation:
-          widget.alumni.alumni_designation, // Replace with the actual alumni designation
+      alumniName: widget.alumni.alumni_name,
+      alumniDesignation: widget.alumni.alumni_designation,
       caption: _organisationNameController.text,
       description: _detailsController.text,
       imageURL: imageURL,
     );
+
+    setState(() {
+      _isLoading = false;
+    });
+
+    // Show a success message
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text('New post added'),
+        duration: Duration(seconds: 2),
+      ),
+    );
+
     _resetForm();
-    // Show a success message or navigate to the home page
   }
 
   @override
@@ -94,124 +109,128 @@ class _AlumniNewPostPageState extends State<AlumniNewPostPage> {
       appBar: AppBar(
         title: const Text('Create Post'),
       ),
-      body: SingleChildScrollView(
-        child: Padding(
-          padding: const EdgeInsets.all(16.0),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              const Text(
-                'Post Type',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 8.0),
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(8.0),
-                  border: Border.all(color: Colors.grey),
-                ),
-                child: DropdownButton<String>(
-                  value: _postType,
-                  isExpanded: true,
-                  underline: const SizedBox.shrink(),
-                  items: _postTypes.map((String value) {
-                    return DropdownMenuItem<String>(
-                      value: value,
-                      child: Text(value),
-                    );
-                  }).toList(),
-                  onChanged: (String? newValue) {
-                    setState(() {
-                      _postType = newValue!;
-                    });
-                  },
-                ),
-              ),
-              const SizedBox(height: 16.0),
-              const Text(
-                'Details',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextField(
-                controller: _detailsController,
-                decoration: const InputDecoration(
-                  hintText: 'Enter details',
-                  border: OutlineInputBorder(),
-                ),
-                maxLines: null,
-              ),
-              const SizedBox(height: 16.0),
-              const Text(
-                'Organisation Name',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              TextField(
-                controller: _organisationNameController,
-                decoration: const InputDecoration(
-                  hintText: 'Enter organisation name',
-                  border: OutlineInputBorder(),
-                ),
-              ),
-              const SizedBox(height: 16.0),
-              const Text(
-                'Photo',
-                style: TextStyle(
-                  fontSize: 18.0,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              const SizedBox(height: 8.0),
-              Row(
-                children: [
-                  IconButton(
-                    onPressed: _pickImage,
-                    icon:Icon(Icons.upload_file)
-                  ),
-                  if (_selectedImage != null)
-                    Expanded(
-                      child: Row(
-                        children: [
-                          const SizedBox(width: 16.0),
-                          Expanded(
-                            child: Image.file(
-                              File(_selectedImage!.path),
-                              height: 200,
-                              fit: BoxFit.contain,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: _cancelImageSelection,
-                            icon: const Icon(Icons.cancel),
-                          ),
-                        ],
+      body: _isLoading
+          ? const Center(
+              child: CircularProgressIndicator(),
+            )
+          : SingleChildScrollView(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Post Type',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                ],
+                    const SizedBox(height: 8.0),
+                    Container(
+                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(8.0),
+                        border: Border.all(color: Colors.grey),
+                      ),
+                      child: DropdownButton<String>(
+                        value: _postType,
+                        isExpanded: true,
+                        underline: const SizedBox.shrink(),
+                        items: _postTypes.map((String value) {
+                          return DropdownMenuItem<String>(
+                            value: value,
+                            child: Text(value),
+                          );
+                        }).toList(),
+                        onChanged: (String? newValue) {
+                          setState(() {
+                            _postType = newValue!;
+                          });
+                        },
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    const Text(
+                      'Details',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextField(
+                      controller: _detailsController,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter details',
+                        border: OutlineInputBorder(),
+                      ),
+                      maxLines: null,
+                    ),
+                    const SizedBox(height: 16.0),
+                    const Text(
+                      'Organisation Name',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextField(
+                      controller: _organisationNameController,
+                      decoration: const InputDecoration(
+                        hintText: 'Enter organisation name',
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                    const SizedBox(height: 16.0),
+                    const Text(
+                      'Photo',
+                      style: TextStyle(
+                        fontSize: 18.0,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 8.0),
+                    Row(
+                      children: [
+                        IconButton(
+                          onPressed: _pickImage,
+                          icon: Icon(Icons.upload_file),
+                        ),
+                        if (_selectedImage != null)
+                          Expanded(
+                            child: Row(
+                              children: [
+                                const SizedBox(width: 16.0),
+                                Expanded(
+                                  child: Image.file(
+                                    File(_selectedImage!.path),
+                                    height: 200,
+                                    fit: BoxFit.contain,
+                                  ),
+                                ),
+                                IconButton(
+                                  onPressed: _cancelImageSelection,
+                                  icon: const Icon(Icons.cancel),
+                                ),
+                              ],
+                            ),
+                          ),
+                      ],
+                    ),
+                    const SizedBox(height: 16.0),
+                    ElevatedButton(
+                      onPressed: _submitPost,
+                      child: const Text('Submit'),
+                    ),
+                    const SizedBox(height: 16.0),
+                    IconButton(
+                      onPressed: _resetForm,
+                      icon: Icon(Icons.delete),
+                    ),
+                  ],
+                ),
               ),
-              const SizedBox(height: 16.0),
-              ElevatedButton(
-                onPressed: _submitPost,
-                child: const Text('Submit'),
-              ),
-              const SizedBox(height: 16.0),
-              IconButton(
-                onPressed: _resetForm,
-                icon:Icon(Icons.delete)
-              ),
-            ],
-          ),
-        ),
-      ),
+            ),
     );
   }
 }
