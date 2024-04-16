d1afd069594575b8987bb1ca76a3b024e0b94164
Akshay-C-A <akshayanil3103@gmail.com>
2024-04-16 11:43:07 +0530

    edited login page

--- edunetdemo/lib/auth/login_page.dart
+++ edunetdemo/lib/auth/login_page.dart
@@ -147,6 +147,9 @@ class _LoginPageState extends State<LoginPage> {
                     },
                     child: Text('Login'),
                   ),
+                  SizedBox(height: 20.0),
+                  Center(child: Text("or")),
+                  SizedBox(height: 20.0),
                   ElevatedButton(
                     onPressed: () async {
                   try {
@@ -159,9 +162,15 @@ class _LoginPageState extends State<LoginPage> {
                   child: Row(
                     mainAxisSize: MainAxisSize.min,
                     children: [
-                      // Image.asset('assets/google_logo.png', height: 24.0),
-                      SizedBox(width: 8.0),
-                      Text('Sign in with Google'),
+                              Image.asset('assets/google.png', height: 24.0),
+                              SizedBox(width: 16.0),
+                              Text(
+                                'Sign in with Google',
+                                style: TextStyle(
+                                  fontSize: 16.0,
+                                  // fontWeight: FontWeight.w400,
+                                )
+                              ),
                     ],
                   ),
               ),
@@ -174,54 +183,3 @@ class _LoginPageState extends State<LoginPage> {
   }
 }
 
-
-
-// import 'package:flutter/material.dart';
-
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:google_sign_in/google_sign_in.dart';
-
-// Future<UserCredential> signInWithGoogle() async {
-//   // Trigger the authentication flow
-//   final GoogleSignInAccount? googleUser = await GoogleSignIn().signIn();
-
-//   // Obtain the auth details from the request
-//   final GoogleSignInAuthentication? googleAuth = await googleUser?.authentication;
-
-//   // Create a new credential
-//   final credential = GoogleAuthProvider.credential(
-//     accessToken: googleAuth?.accessToken,
-//     idToken: googleAuth?.idToken,
-//   );
-
-//   // Once signed in, return the UserCredential
-//   return await FirebaseAuth.instance.signInWithCredential(credential);
-// }
-
-// class GoogleSignInButton extends StatelessWidget {
-//   const GoogleSignInButton({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return Center(
-//       child: ElevatedButton(
-//         onPressed: () async {
-//           try {
-//             await signInWithGoogle();
-//             // Handle successful sign-in
-//           } catch (e) {
-//             // Handle sign-in error
-//           }
-//         },
-//           child: Row(
-//             mainAxisSize: MainAxisSize.min,
-//             children: [
-//               // Image.asset('assets/google_logo.png', height: 24.0),
-//               SizedBox(width: 8.0),
-//               Text('Sign in with Google'),
-//             ],
-//           ),
-//       ),
-//     );
-//   }
-// }
