7d798403b2d0a4f15f732da9dd2177e97cc461f5
AmiyaHidayathulla <amiyahiday208@gmail.com>
2024-04-26 21:52:48 +0530

    updated

--- edunetdemo/lib/student/student_profile.dart
+++ edunetdemo/lib/student/student_profile.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:edunetdemo/auth/login_check.dart';
 import 'package:edunetdemo/services/firestore.dart';
 import 'package:edunetdemo/student/student_dashboard.dart';
 import 'package:edunetdemo/student/student_edit-profile.dart';
@@ -39,6 +40,11 @@ class _ProfileScreenState extends State<ProfileScreen> {
               onPressed: () async {
                 try {
                   await FirebaseAuth.instance.signOut();
+                  Navigator.pushAndRemoveUntil(
+                    context,
+                    MaterialPageRoute(builder: (context) => MainPage()),
+                    (route) => false,
+                  );
                 } catch (e) {
                   print('Error signing out: $e');
                 }
