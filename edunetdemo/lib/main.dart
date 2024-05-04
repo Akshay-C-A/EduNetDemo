f66ec209f20f03fe2ff04a5f3699d4ce77dd46d7
Kennith Joseph <sendmeamailatkennithjoseph@gmail.com>
2024-05-05 00:32:27 +0530

    Added intro page successfully

--- edunetdemo/lib/main.dart
+++ edunetdemo/lib/main.dart
@@ -1,9 +1,11 @@
 import 'package:edunetdemo/auth/login_check2.dart';
+import 'package:edunetdemo/onboarding/onboarding_screen.dart';
 import 'package:edunetdemo/services/notification_services.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'firebase_options.dart';
 
 void main() async {
@@ -18,7 +20,38 @@ void main() async {
   await NotificationService().initNotification();
 
   await FirebaseAppCheck.instance.activate();
-  runApp(MaterialApp(
-    home: const MainPage(),
-  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'My App',
+      home: FutureBuilder<bool>(
+        future: _checkIfFirstLaunch(),
+        builder: (BuildContext context, AsyncSnapshot<bool> snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return CircularProgressIndicator();
+          } else if (snapshot.hasData && snapshot.data!) {
+            return OnBoardingScreen();
+          } else {
+            return MainPage();
+          }
+        },
+      ),
+    );
+  }
+
+  Future<bool> _checkIfFirstLaunch() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool isFirstLaunch = prefs.getBool('isFirstLaunch') ?? true;
+    if (isFirstLaunch) {
+      prefs.setBool('isFirstLaunch', false);
+    }
+    return isFirstLaunch;
+  }
 }
